83dacac18da7ba1691ed7373e1453394d5e140c1
Owain Williams <104087665+owain-williams@users.noreply.github.com>
2022-07-04 17:01:36 +0100

    added routing and blog display

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+posts_endpoint = "https://api.npoint.io/f74d23408742b9aea05a"
+all_posts = requests.get(posts_endpoint).json()
+
 @app.route("/")
 def get_home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 @app.route("/about")
 def about():
